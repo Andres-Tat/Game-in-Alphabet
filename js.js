4371dd6408cc3083935f2e48d9057028b16f830d
Andres Tatarnikov <119009469+Andres-Tat@users.noreply.github.com>
2024-03-29 21:59:32 +0300

    add comment and modernize yout code

--- js.js
+++ js.js
@@ -1,14 +1,35 @@
-"use strict";
+'use strict';
 
-// Получаем доступ к элементам по селекторам
-let letterA = document.querySelector(".box_a");
-let letterB = document.querySelector(".box_b");
-let letterC = document.querySelector(".box_c");
-let letterD = document.querySelector(".box_d");
-let letterE = document.querySelector(".box_e");
-let letterF = document.querySelector(".box_f");
+// // Получаю доступ ко всем элементам по селектору, получил путь к файлу аудио и присвоил его в переменную, создал пустую переменную 'newLetters'.
+const letters = document.querySelectorAll('.boxes__box');
+const audioClick = new Audio('audio/Mountain Audio - Menu Click.mp3');
+const audioWin = new Audio('audio/huge win.wav');
+let newLetters;
 
-
-letterA.addEventListener('click', function () {
-    letterA.previousElementSibling.before(letterA)
-} )
+// Использую цикл 'for if', 'for else if', добавляю обработчик событий для массива 'letters' и прописываю функциональные выражения с использованием 'previousElementSibling' и 'nextElementSibling', добавляю проигрывание музыки после каждого события, , использую переменную 'newLetters' которая собирает в себя результат события 'previousElementSibling' и 'nextElementSibling'.
+for (let i = 0; i < letters.length; i++) {
+  letters[i].addEventListener('click', function () {
+    if (letters[i].previousElementSibling != null) {
+      letters[i].previousElementSibling.before(letters[i]);
+      audioClick.play();
+      newLetters = document.querySelectorAll('.boxes__box');
+      console.log(newLetters);
+    } else if (letters[i].nextElementSibling != null) {
+      letters[i].nextElementSibling.after(letters[i]);
+      audioClick.play();
+      newLetters = document.querySelectorAll('.boxes__box');
+      console.log(newLetters);
+    }
+    if (
+      // создал условие, при котором метод '.contains' проверяет, совпали ли классы с индексом массива и выполняет условие, при котором будет играть музыка.
+      newLetters[0].classList.contains('box_a') &&
+      newLetters[1].classList.contains('box_b') &&
+      newLetters[2].classList.contains('box_c') &&
+      newLetters[3].classList.contains('box_d') &&
+      newLetters[4].classList.contains('box_e') &&
+      newLetters[5].classList.contains('box_f')
+    ) {
+      audioWin.play();
+    }
+  });
+}
